ccffbc85ed0e604652638362f224e95435a7101f
hoony00 <nasi3611@naver.com>
2023-11-28 20:52:32 +0900

    feat - 바텀네비게이션 지정

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 
 enum TabItem {
   home(Icons.home, '홈', HomeFragment()),
-  favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
+  localLife(Icons.holiday_village_outlined, '동네생활', FavoriteFragment(isShowBackButton: false)),
+  nearMe(Icons.pin_drop_outlined, '내 근처', FavoriteFragment(isShowBackButton: false)),
+  chat(Icons.chat_bubble_outline, '채팅', FavoriteFragment(isShowBackButton: false)),
+  my(Icons.person_outline_rounded, '나의 당근', FavoriteFragment(isShowBackButton: false));
 
   final IconData activeIcon;
   final IconData inActiveIcon;
