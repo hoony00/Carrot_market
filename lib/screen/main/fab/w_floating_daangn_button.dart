14e9e44919f6eca76a4c78809456bcc18f272419
hoony00 <nasi3611@naver.com>
2023-11-28 21:53:46 +0900

    feat - animated 이용, 플로팅 버튼 액션 처리

--- lib/screen/main/fab/w_floating_daangn_button.dart
+++ lib/screen/main/fab/w_floating_daangn_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hoonyDev/common/common.dart';
 import 'package:hoonyDev/common/dart/extension/num_duration_extension.dart';
+import 'package:hoonyDev/common/widget/animated_width_collapse.dart';
 import 'package:hoonyDev/screen/main/s_main.dart';
 
 class FloatingDaangnButton extends ConsumerWidget {
@@ -12,12 +13,13 @@ class FloatingDaangnButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isExpanded = true;
+    final isSmall = true;
     return Stack(
       children: [
         AnimatedContainer(
           duration: duration,
           color:
-              isExpanded ? Colors.black.withOpacity(0.4) : Colors.transparent,
+          isExpanded ? Colors.black.withOpacity(0.4) : Colors.transparent,
         ),
         Align(
           alignment: Alignment.bottomRight,
@@ -25,40 +27,52 @@ class FloatingDaangnButton extends ConsumerWidget {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              Container(
-                width: 160,
-                padding: const EdgeInsets.all(15),
-                margin: const EdgeInsets.only(bottom: 10, right: 15),
-                decoration: BoxDecoration(
-                  color: context.appColors.floatingActionLayer,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _floatItem('알바', 'assets/image/fab/fab_01.png'),
-                    _floatItem('과외/클래스', 'assets/image/fab/fab_02.png'),
-                    _floatItem('농수신믈', 'assets/image/fab/fab_03.png'),
-                    _floatItem('부동산', 'assets/image/fab/fab_04.png'),
-                    _floatItem('중고차', 'assets/image/fab/fab_05.png'),
-                  ],
+              AnimatedOpacity(
+                opacity: isExpanded ? 1 : 0,
+                duration: duration,
+                child: Container(
+                  width: 160,
+                  padding: const EdgeInsets.all(15),
+                  margin: const EdgeInsets.only(bottom: 10, right: 15),
+                  decoration: BoxDecoration(
+                    color: context.appColors.floatingActionLayer,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _floatItem('알바', 'assets/image/fab/fab_01.png'),
+                      _floatItem('과외/클래스', 'assets/image/fab/fab_02.png'),
+                      _floatItem('농수신믈', 'assets/image/fab/fab_03.png'),
+                      _floatItem('부동산', 'assets/image/fab/fab_04.png'),
+                      _floatItem('중고차', 'assets/image/fab/fab_05.png'),
+                    ],
+                  ),
                 ),
               ),
               AnimatedContainer(
                 duration: duration,
                 height: 60,
+                padding: const EdgeInsets.symmetric(horizontal: 18),
                 decoration: BoxDecoration(
-                  color: Color(0xffff791f),
+                  color:
+                  isExpanded
+                      ? context.appColors.floatingActionLayer
+                      : const Color(0xffff791f),
                   borderRadius: BorderRadius.circular(30),
                 ),
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    const Icon(Icons.add),
-                    '글쓰기'.text.make(),
+                    AnimatedRotation(turns: isExpanded ? 0.125 : 0, duration: duration, child: const Icon(Icons.add)),
+                    AnimatedWidthCollapse(
+                        visible: !isSmall,
+                        duration: duration,
+                        child: '글쓰기'.text.make()),
                   ],
                 ),
-              ).pOnly(bottom: MainScreenState.bottomNavigationBarHeight+ context.viewPaddingBottom, right: 20),
+              ).pOnly(bottom: MainScreenState.bottomNavigationBarHeight +
+                  context.viewPaddingBottom, right: 20),
             ],
           ),
         ),
@@ -71,6 +85,7 @@ class FloatingDaangnButton extends ConsumerWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(imagePath, width: 30),
+        const Width(8),
         title.text.make(),
       ],
     );
